56a7711a4a69ba98bdb05f5b85539d84086c97e7
jihwan629 <jihwan629@naver.com>
2021-01-10 14:58:37 +0900

    추가 업로드

---
+++ Chapter20_5_Client/Chapter20_5_Client/Chapter20_5_Client.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	try
+	{
+		if (argc != 2)
+		{
+			cerr << "Usage: client <host>" << endl;
+		}
+
+		tcp::iostream stream(argv[1], std::to_string(int(13))); // port number 13
+
+		if (!stream)
+		{
+			cout << "Unable to connect : "
+				<< stream.error().message() << endl;
+			return 1;
+		}
+
+		// send message to server
+		stream << "Hello from client";
+		stream << endl; // send new-line to end receiver' stream getline
+
+		// receive message from server
+		std::string line;
+		std::getline(stream, line);
+		cout << line << endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << "Exception : " << e.what() << std::endl;
+	}
+    return 0;
+}
---
+++ Chapter20_5_Serveer/Chapter20_5_Serveer/Chapter20_5_Serveer.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <ctime>
+#include <string>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+using namespace std;
+
+int main()
+{
+	try
+	{
+		boost::asio::io_service io_service;
+
+		tcp::endpoint endpoint(tcp::v4(), 13);
+		tcp::acceptor acceptor(io_service, endpoint);
+
+		std::cout << "Server started" << endl;
+
+		for (;;) // while(true)
+		{
+			const std::string message_to_send = "Hello From Server";
+
+			boost::asio::ip::tcp::iostream stream;
+
+			cout << "check 1" << endl;
+
+			boost::system::error_code ec;
+			acceptor.accept(*stream.rdbuf(), ec);
+			// 클라이언트의 read buffer 읽음
+
+			cout << "check 2" << endl;
+
+			if (!ec) // 클라이언트가 접속이 됬다면
+			{
+				// receive message from client
+				std::string line;
+				std::getline(stream, line);
+				cout << line << endl;
+
+				// send message to client
+				stream << message_to_send;
+				stream << endl; // send std::endl to end getline of client
+			}
+
+		}
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
